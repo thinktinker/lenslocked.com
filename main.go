eb3473e75e2211e44c5a0ccb68d39238c984a4cc
Martin Leong <martinleong@Martins-SpaceGrey-2017-MB.local>
2020-12-03 22:12:11 +0800

    Added a contact page, used request.URL.Path

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html") //or you can you text/plain
-	fmt.Fprint(w, "<h1>Welcome to my web site</h1>")
+	// fmt.Fprint(w, r.URL.Path)                //r.URL.Path shows the current URL Path requested
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my web site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Get in touch with us. Send an email to <a href=\"mailto:support@lenslocked.com\">lenslocked.com</a>")
+	}
+
 }
 
 func main() {
