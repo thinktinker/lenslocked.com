b22c9be8f338a0119307f7369b08b15efda5ca05
Martin Leong <martinleong@Martins-SpaceGrey-2017-MB.local>
2020-12-06 01:27:33 +0800

    Added a 404 Page for a simple router

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my web site</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "Get in touch with us. Send an email to <a href=\"mailto:support@lenslocked.com\">lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for.</h1>")
 	}
 
 }
