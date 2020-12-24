fda948f6f65fc80b3dfc8cd84548f7817324f42b
Martin Leong <martinleong@Martins-SpaceGrey-2017-MB.local>
2020-12-24 23:07:04 +0800

    Created table records in gorm

--- expdb/main.go
+++ expdb/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	// using gorm instead to access Go's database/sql package
 	// gorm is an Object Relation Mapper or a code library that automates
@@ -15,7 +18,7 @@ const (
 	port     = 5432
 	user     = "<username>" // username here uid -UN
 	password = ""
-	dbname   = "<database_name>" //database name here
+	dbname   = "database_name" //database name here
 )
 
 //Create a GORM model
@@ -23,6 +26,7 @@ type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+	Color string
 }
 
 func main() {
@@ -50,9 +54,45 @@ func main() {
 		panic(err)
 	}
 
+	//Run the log mode to see the queries run by Gorm's AutoMigrate
+	db.LogMode(true)
+
 	//use Automigrate to gererate the Usertable based on the struct Model created above
-	// db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{})
+
+	name, email, color := getInfo()
+
+	u := User{
+		Name:  name,
+		Email: email,
+		Color: color,
+	}
+
+	if err := db.Create(&u).Error; err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", u)
 
 	//use gorm to drop the users table
-	db.DropTableIfExists(&User{})
+	// db.DropTableIfExists(&User{})
+}
+
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+
+	fmt.Println("What is your favourite color?")
+	color, _ = reader.ReadString('\n')
+
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+
+	return name, email, color
 }
